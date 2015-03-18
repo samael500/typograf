78b11bbdab0903556e0927bf9dd994dfe88172c9
Samael500 <samael500@gmail.com>
2015-03-18 12:15:44 +0300

    :rocket: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='typograf',
+    version='0.1',
+    description='Client for atrlebedev typograf service',
+    url='https://github.com/Samael500/typograf',
+    author='Maksim Skorokhod',
+    author_email='Samael500@gmail.com',
+    license='MIT',
+    packages=['typograf'],
+    zip_safe=False
+)
